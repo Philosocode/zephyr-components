1f5d8a6b907c9c906cc29882705fe48e9b23c9b7
Tam Le <philosocode@gmail.com>
2020-09-29 17:54:50 -0600

    Project setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld"
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    // HelloWorld
   }
-};
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+:root {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%; // 1rem = 10px, 10/16 = 62.5%
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: $font-primary;
+  font-size: 1.6rem;
+  line-height: 1.5;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  lintOnSave: false
-};
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `
+          @import "~@/scss/_colors.scss";
+          @import "~@/scss/_fonts.scss";
+          @import "~@/scss/_respond.scss";
+        `
+      }
+    }
+  }
+}
